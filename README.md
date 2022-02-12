bad0261985bbcdb4c964686dda208265783c5089
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2022-02-12 18:52:02 -0500

    made tests

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-# 2-2 Exercise 2
+# 2-2 Exercise 3
 
 ## The assignment
 Edit the file named "student.py".  
 You should only use one print statement, otherwise use the input statement for user instructions.  
   
-Ask the user to enter a number and output the following:  
-
-“Even” if a number entered is even or “Odd” if the number is odd.  
-
+Ask the user to enter a name and output  “This name is in the first half of the alphabet” if the name entered starts with ‘M’ or a letter that comes before M.  
+Or it outputs “This name is in the last half of the alphabet if it starts with N or later.  
+Make sure to test is with a name like Maurice, Mary, Mick, Mohammed, or Mzzzz. All of these names are considered the first part of the alhpabet.  
+  
+Assume the first letter is a capital 'M' since it's a name.
   
 
 ## Check your work
--- student_test.py
+++ student_test.py
@@ -3,8 +3,8 @@ import student
 
 
 
-def test_simple_even():
-    input_values=['2']
+def test_simple_later():
+    input_values=['Zorp']
     output=[]
 
     def mock_input(s=None):
@@ -20,10 +20,10 @@ def test_simple_even():
 
     student.main()
 
-    assert "even" in output[1].lower()
+    assert "is in the last half of the alphabet" in output[1].lower()
 
-def test_negative_even():
-    input_values=['-2']
+def test_simple_first():
+    input_values=['Alice']
     output=[]
 
     def mock_input(s=None):
@@ -39,10 +39,10 @@ def test_negative_even():
 
     student.main()
 
-    assert "even" in output[1].lower()
+    assert "is in the first half of the alphabet" in output[1].lower()
 
-def test_simple_odd():
-    input_values=['5']
+def test_M_name():
+    input_values=['Munich']
     output=[]
 
     def mock_input(s=None):
@@ -58,49 +58,11 @@ def test_simple_odd():
 
     student.main()
 
-    assert "odd" in output[1].lower()
+    assert "is in the first half of the alphabet" in output[1].lower()
 
-def test_negative_odd():
-    input_values=['-9']
-    output=[]
-
-    def mock_input(s=None):
-        if s is not None:
-            output.append(s)
-            return input_values.pop(0)
-        else:
-            output.append("")
-            return input_values.pop(0)
-    
-    student.input = mock_input
-    student.print = lambda s : output.append(s)
-
-    student.main()
-
-    assert "odd" in output[1].lower()
-
-def test_zero_case():
-    input_values=['0']
-    output=[]
-
-    def mock_input(s=None):
-        if s is not None:
-            output.append(s)
-            return input_values.pop(0)
-        else:
-            output.append("")
-            return input_values.pop(0)
-    
-    student.input = mock_input
-    student.print = lambda s : output.append(s)
-
-    student.main()
-
-    assert "even" in output[1].lower()
-
-def test_multi_odd_case():
-    for i in [-9, -3, -31, -41, -99, -19, 87, 19, 3]:
-        input_values=[str(i)]
+def test_multi_first_half():
+    for i in ["Ally","Alexander","Bruce","Lisa","Elizabeth","Kevin","Hector","Luke"]:
+        input_values=[i]
         output=[]
 
         def mock_input(s=None):
@@ -116,4 +78,44 @@ def test_multi_odd_case():
 
         student.main()
 
-        assert "odd" in output[1].lower()
+        assert "is in the first half of the alphabet" in output[1].lower()
+
+def test_multi_last_half():
+    for i in ["Naury","Nickey","Zoey","Octavia","Prince","Queen","Rick","Steve", "Trevor", "Tristan", "Utopia", "Walter","Yussef","Xandria"]:
+        input_values=[i]
+        output=[]
+
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        
+        student.input = mock_input
+        student.print = lambda s : output.append(s)
+
+        student.main()
+
+        assert "is in the last half of the alphabet" in output[1].lower()
+
+def test_multi_M_names():
+    for i in ["M", "Maury","Mickey","Money","Mona-Lisa","Mini","Mohammed","Morty","Mzzzzzzzzzz"]:
+        input_values=[i]
+        output=[]
+
+        def mock_input(s=None):
+            if s is not None:
+                output.append(s)
+                return input_values.pop(0)
+            else:
+                output.append("")
+                return input_values.pop(0)
+        
+        student.input = mock_input
+        student.print = lambda s : output.append(s)
+
+        student.main()
+
+        assert "is in the first half of the alphabet" in output[1].lower()
